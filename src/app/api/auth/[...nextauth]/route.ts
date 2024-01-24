1fd566b1b8a675cea015464794d9100d86a40b85
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-01-24 19:43:01 +0100

    created api route to talk with gh

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+// This implements API's request handlers inside the APP
+
+// If you ever have some outside web service that needs to talk to your next app
+// You can create a route handler like this to build a GET request & a POST request handler
+
+import { GET,POST } from "@/auth";
+
+
