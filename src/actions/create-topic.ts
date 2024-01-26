29f8d772575f6346dedf32f33558b018eda17ac9
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-01-26 19:54:42 +0100

    added useStateForm hook for the createTopicForm

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,19 +1,32 @@
 "use server";
 
-import {z} from "zod";
+import { z } from "zod";
 
 const createTopicSchema = z.object({
-    name:z.string().min(3).regex(/^[a-z-]+$/,{message:"Must be lowercase letters or dashes without spaces"}),
+    name: z.string().min(3).regex(/^[a-z-]+$/, { message: "Must be lowercase letters or dashes without spaces" }),
     description: z.string().min(10)
 })
 
-export async function createTopic(formData: FormData) {
-const result = createTopicSchema.safeParse({
-    name: formData.get("name"),
-    description: formData.get("description")
-})
-if(!result.success){
-    console.log(result.error)
+interface CreateTopicFormState {
+    errors: {
+        name?: string[]
+        description?: string[]
+    }
 }
-//TODO: Revalidate de homepage after creating a topic
+
+export async function createTopic( formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
+    const result = createTopicSchema.safeParse({
+        name: formData.get("name"),
+        description: formData.get("description")
+    })
+    if (!result.success) {
+        return {
+            errors: result.error.flatten().fieldErrors
+        }
+    }
+
+    return {
+        errors: {}
+    }
+    //TODO: Revalidate de homepage after creating a topic
 }
