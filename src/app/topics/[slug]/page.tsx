9d64c48a3195b05db1d80aab64bf993513a457a4
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-01-28 20:51:52 +0100

    added dinamic slug inTopicShowPage

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-const TopicShowPage = () => {
-  return (
-    <div>TopicShowPage</div>
-  )
+import React from "react";
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
 }
 
-export default TopicShowPage
+const TopicShowPage = ({ params }: TopicShowPageProps) => {
+  const { slug } = params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb2">{slug}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default TopicShowPage;
